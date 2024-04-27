9ed40b82fb9ac7836fc6b10bb600464c64373ca4
Rajesh1215 <126228085+Rajesh1215@users.noreply.github.com>
2024-04-27 16:23:22 +0530

    done

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,20 +1,16 @@
 export const Bio = {
-  name: "Rishav Chanda",
+  name: "Rajesh kammaluri",
   roles: [
-    "Full Stack Developer",
-    "Android Developer",
-    "UI/UX Designer",
-    "Programmer",
+    "ML/DL Engineer",
+    "Full Stack developer,Flutter Developer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
-  resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
-  facebook: "https://www.facebook.com/rishav.chanda.165/",
+  github: "https://github.com/Rajesh1215",
+
+  linkedin: "https://www.linkedin.com/in/rajaiguy/",
+  twitter: "https://twitter.com/Kingrajesh1215",
+
 };
 
 export const skills = [
